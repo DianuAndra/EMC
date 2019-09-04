65cc5f9a104c9847817c0e2fe3df300f236fbe30
DianuAndra <dianuandra@gmail.com>
2019-09-04 14:45:44 +0300

    Added Logging & Unit test proj

--- ExamenEMC/ExamenEMC/Source.cpp
+++ ExamenEMC/ExamenEMC/Source.cpp
@@ -1,10 +1,19 @@
-#include<iostream>
+#include <iostream>
+#include "../Logging/Logging1.h"
 #include <string>
+#include <fstream>
 
-void main() {
+int main() {
+
+	std::ofstream of("syslog.log", std::ios::app);
+	Logger logger(of);
+	logger.log("Started Application...", Logger::Level::Info);
+
 	std::string str1= "jel";
 	std::cout << "HEllp" << std::endl;
 	std::cout << str1;
 
 	system("pause");
+
+	return 0;
 }
